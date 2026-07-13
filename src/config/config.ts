bb0614b0653b2ad43e4bb9b12ce62729d41a885f
Wout Salembier <wout.salembier@hotmail.com>
2026-07-13 14:30:01 +0200

    feat: add Redis and PostgreSQL support with necessary dependencies

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,6 @@
 export const config = () => ({
-    port: 3000,
-    jwtSecret:  process.env.JWT_SECRET || 'your_jwt_secret',
+  port: 3000,
+  jwtSecret: process.env.JWT_SECRET || "your_jwt_secret",
+  redisUrl: process.env.REDIS_URL || "redis://localhost:6379",
+databaseUrl: process.env.DATABASE_URL || "postgres://"
 });
